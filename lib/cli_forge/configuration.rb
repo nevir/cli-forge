d4d13fa7e9a7dc63135dc17f29582e16a1fb3f66
Ian MacLeod <ian@nevir.net>
2013-03-02 22:58:34 -0800

    Stub out config and runner

---
+++ lib/cli_forge/configuration.rb
@@ -0,0 +1,7 @@
+class CLIForge::Configuration
+
+  # The name of the binary for this program; used to determine where to find
+  # sub-commands, help, etc.
+  attr_accessor :bin_name
+
+end
---
+++ lib/cli_forge/runner.rb
@@ -0,0 +1,11 @@
+class CLIForge::Runner
+
+  def initialize(config)
+    @config = config
+  end
+
+  def start
+
+  end
+
+end
